43403ff9306843be617c57c79678f9347d2edf99
Érick Augusto Paludo <erickpaludoyt@gmail.com>
2024-06-07 00:22:13 -0300

    hhhhhhhhh

--- obj/Debug/net6.0-windows/Vanilla.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/Vanilla.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vanilla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98322c528edf0b1625013308965ed64da4340a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vanilla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vanilla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
